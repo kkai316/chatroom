7e2ac9637cb8a7e7d81a3f99af77475b523a3a06
Kai-Lea <kaileawu316@gmail.com>
2016-10-20 14:26:01 -0500

    add my project

---
+++ index.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  //res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/main.html');
+
+});
+
+io.on('connection', function(socket){
+  console.log('hello user!, a user connected');
+  socket.broadcast.emit('everyone');
+
+  /*socket.on('input_name', function(name){
+    console.log('name: ' + name);
+    io.emit('input_name', name);
+  });*/
+  
+  
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('------user disconnected-----');
+    io.emit('leave notice');
+  });
+
+  /*socket.on('connection', function(){
+    io.emit('connect');
+  });*/
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
