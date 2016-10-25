b3c981c468c59eba48872ed5904c88500cbfcc17
Kai-Lea <kaileawu316@gmail.com>
2016-10-25 14:11:03 -0500

    add body-parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+
 
 app.get('/', function(req, res){
   //res.sendFile(__dirname + '/index.html');
