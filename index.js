5f480f85a6e7337ad489ca8568ed29f1d53bab18
Kai-Lea <kaileawu316@gmail.com>
2016-10-26 10:56:29 -0500

    solve duplicate connecting msg and display user name

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
-
-
 
 app.get('/', function(req, res){
-  //res.sendFile(__dirname + '/index.html');
-  //console.log(res);
-  //res.send('Username: ' + req.query['username']);
   res.sendFile(__dirname + '/chat.html');
-
-});
-
-app.post('/', function(req, res){
-  //res.sendFile(__dirname + '/index.html');
-  console.log(res.body);
-
 });
 
 io.on('connection', function(socket){
   console.log('hello user!, a user connected');
-  socket.broadcast.emit('everyone');
   
+  //when join the chat room
+  socket.on('new user', function(name){
+  	socket.username = name;
+  	console.log(name + 'logged');
+  	socket.broadcast.emit('new user', {username: name});
+  });
+  
+  //chat action
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 
+  //leave chatroom
   socket.on('disconnect', function(){
     console.log('------user disconnected-----');
     io.emit('leave notice');
   });
-
 
 });
 
